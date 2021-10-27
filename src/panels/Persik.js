d844e3bf1675ef3c058dbcee88ec1fc5fb184ec9
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-27 19:07:09 +0300

    Update Persik.js

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -19,13 +19,13 @@ const Persik = props => (
 		<img className="Persik"  src={persik} alt="Котперсик"/>
 		<Group  header={<Header className="Persik" mode="primary">КАКАЯ ТЫ ГАВКА?</Header>}>
 			<Div>
-				<Button  className="ButStyle" stretched size="l" mode="secondary" onClick={props.go} data-to="panel3">
+				<Button   stretched size="l" mode="secondary" onClick={props.go} data-to="panel3">
 					НЕБОЛЬШАЯ
 				</Button>
-				<Button className="ButStyle" stretched size="l" mode="secondary" onClick={props.go} data-to="panel3">
+				<Button  stretched size="l" mode="secondary" onClick={props.go} data-to="panel3">
 					НЕМАЛЕНЬКАЯ
 				</Button>
-				<Button  className="Persik" mode="secondary" onClick={props.go} data-to="panel3">
+				<Button   mode="secondary" onClick={props.go} data-to="panel3">
 					Далее
 				</Button>
 			</Div>
