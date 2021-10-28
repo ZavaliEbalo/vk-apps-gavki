2a36492c5356d4b3a5e8ad6af33c4e5dfcf362ff
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-29 00:59:41 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -5,17 +5,19 @@ import PropTypes from 'prop-types';
 import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
 
 import './TextHomeHeader.css';
+import './TextHeaderGay.css';
+import './PanelBackground.css';
 
 const Home = ({ id, go }) => (
 
-	<Panel id={id}>
+	<Panel className="PanelBackground" id={id}>
 
-		<PanelHeader className="TextHomeHeader" >УЗНАЙ КАКАЯ ТЫ СОБАКА</PanelHeader>
+		<PanelHeader className="TextHeaderGay"  >УЗНАЙ КАКАЯ ТЫ СОБАКА</PanelHeader>
 
-		<Group header={<Header mode="secondary">Пройди тест и узнай, какая ты собака прямой сейчас</Header>}>
+		<Group className="TextHomeHeader"  header={<Header mode="primary" subtitle="(бесплатно без смс и регистрации)">Пройди тест и узнай, какая ты собака прямо сейчас!</Header>}>
 			<Div>
 				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					ПРОЙТИ ТЕСТ
+					ПРОЙТИ ТЕСТ 🢫
 				</Button>
 			</Div>
 		</Group>
