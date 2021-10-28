36417a9b39f2abede95148a59720bc759b64338b
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-28 23:16:50 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
 
 import './TextHomeHeader.css';
-//Добавить сюда fetchedUser
+
 const Home = ({ id, go }) => (
 
 	<Panel id={id}>
-		<PanelHeader className="TextHomeHeader">УЗНАЙ КАКАЯ ТЫ СОБАКА</PanelHeader>
 
-	// 	{fetchedUser &&
-	// 	<Group header={<Header mode="secondary">Тестируемая гавка:</Header>}>
-	// 		<Cell
-	// 			before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-	// 			description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-	// 		>
-	// 			{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-	// 		</Cell>
-	// 	</Group>
-	// }
-<Div>
+		<PanelHeader >УЗНАЙ КАКАЯ ТЫ СОБАКА</PanelHeader>
+
 		<Group header={<Header mode="secondary">Пройди тест и узнай, какая ты собака прямой сейчас</Header>}>
 			<Div>
 				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
@@ -28,7 +19,7 @@ const Home = ({ id, go }) => (
 				</Button>
 			</Div>
 		</Group>
-		</Div>
+
 	</Panel>
 
 );
@@ -36,14 +27,6 @@ const Home = ({ id, go }) => (
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
-	// fetchedUser: PropTypes.shape({
-	// 	photo_200: PropTypes.string,
-	// 	first_name: PropTypes.string,
-	// 	last_name: PropTypes.string,
-	// 	city: PropTypes.shape({
-	// 		title: PropTypes.string,
-	// 	}),
-	// }),
 };
 
 export default Home;
