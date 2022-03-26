00c4d83dd05f8c4621111ad46bb1eda58dbb35fb
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2022-03-26 19:07:16 +0300

    Create Panel7.js

---
+++ src/panels/Panel7.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Panel, PanelHeader, PanelHeaderBack, Button, Group,  Div, Header, Placeholder } from '@vkontakte/vkui';
+
+import persik from '../img/persik.png';
+import spotti6 from '../img/spotti6.png';
+import './Persik.css';
+
+import './Persik.css';
+import './ButStyle.css';
+import './block.css';
+import './BacButtonDalee.css';
+import './GradientTest.css';
+import './ButtonSelectedOne.css';
+import './form-radio.css';
+import './BacButtonStyle.css';
+import './bac.css';
+
+const Panel7 = props => {
+
+	return (
+
+	<Panel className="PanelBackground" id={props.id}>
+		<PanelHeader
+			left={<PanelHeaderBack onClick={props.go} data-to="panel6"/>}
+		>
+
+		</PanelHeader>
+
+		<Group  header={<Header className="Panel7" mode="primary" ></Header>}>
+
+			<Div >
+
+			<Placeholder
+				header="УРА! ТЕСТ ПРОЙДЕН!"
+			>
+			<img   src={spotti6} alt="Котперсик"/>
+
+			</Placeholder>
+
+
+							 <Button    id="btn1" className="BacButtonStyle" stretched size="l"  mode="secondary" onClick={props.go} data-to="panel8" >
+			 				 УЗНАТЬ РЕЗУЛЬТАТЫ
+			 				</Button>
+
+
+
+
+
+
+
+			</Div>
+
+		</Group>
+
+	</Panel>
+);
+}
+
+Panel7.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+};
+
+export default Panel7;
