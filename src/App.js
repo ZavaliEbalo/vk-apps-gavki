5c35741c2d96acfb63e1cb72b36b2100b98fb578
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2022-03-26 19:06:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import '@vkontakte/vkui/dist/vkui.css';
 import Home from './panels/Home';
 import Persik from './panels/Persik';
 import Panel3 from './panels/Panel3';
+import Panel4 from './panels/Panel4';
+import Panel5 from './panels/Panel5';
+import Panel6 from './panels/Panel6';
+import Panel7 from './panels/Panel7';
+import Panel8 from './panels/Panel8';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
@@ -36,6 +41,11 @@ const App = () => {
 					<Home id='home' go={go}  />
 					<Persik id='persik' go={go} />
 					<Panel3 id='panel3' go={go} />
+					<Panel4 id='panel4' go={go} />
+					<Panel5 id='panel5' go={go} />
+					<Panel6 id='panel6' go={go} />
+					<Panel7 id='panel7' go={go} />
+					<Panel8 id='panel8' go={go} />
 				</View>
 			</AppRoot>
 		</AdaptivityProvider>
